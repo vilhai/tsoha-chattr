3f5ff5564bf77b3b3c2b0574e362d7b421d5ae27
Häikiö Vili A <vilihaik@cubbli22-gold.local>
2024-08-18 21:32:36 +0300

    Add sql file for testing the app

---
+++ testdata.sql
@@ -0,0 +1,4 @@
+INSERT INTO topics (name) VALUES ("General");
+INSERT INTO topics (name) VALUES ("Just talking");
+INSERT INTO topics (name) VALUES ("SQL problems");
+INSERT INTO topics (name) VALUES ("Flask");
