621bbce1dfacdea2857debb0a53dd02e5ede6717
Häikiö Vili A <vilihaik@cubbli22-gold.local>
2024-09-01 02:44:38 +0300

    Correct message order

--- messages.py
+++ messages.py
@@ -5,7 +5,7 @@ import users
 import topics
 
 def get_messages(topic_id):
-    sql = text("SELECT M.id, M.content, M.time_sent, U.username FROM messages M, users U WHERE U.id = M.user_id AND M.topic_id = :topic_id")
+    sql = text("SELECT M.id, M.content, M.time_sent, U.username FROM messages M, users U WHERE U.id = M.user_id AND M.topic_id = :topic_id ORDER BY time_sent")
     res = db.session.execute(sql, {"topic_id": topic_id})
     return res.fetchall()
 
